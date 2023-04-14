5e29f21989b01f05edf1583fb3592ad7578eb9cc
Zachary Young <zhyoung@iu.edu>
2023-04-14 16:12:44 -0400

    Initial commit

---
+++ src/main/java/edu/c322final/receiptservice/ReceiptserviceApplication.java
@@ -0,0 +1,13 @@
+package edu.c322final.receiptservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/c322final/receiptservice/ReceiptserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.c322final.receiptservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
